2fe0dee1b57bd33341c0c495f37838fea7ac2087
keemeesuu <kimhogy@gmail.com>
2021-04-27 23:18:15 +0900

    Initial Commit

---
+++ Classroom/ClassroomApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClassroomApp.swift
+//  Classroom
+//
+//  Created by hogy on 2021/04/27.
+//
+
+import SwiftUI
+
+@main
+struct ClassroomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
